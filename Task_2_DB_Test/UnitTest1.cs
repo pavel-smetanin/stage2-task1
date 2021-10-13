23cd884b0572c4d31ce1f5e7ce1cf5bca869560e
pavel-smetanin <pavel.smetanin.01@mail.ru>
2021-10-13 19:16:00 +0300

    Created a new project

---
+++ Task_2_DB_Test/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace Task_2_DB_Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
